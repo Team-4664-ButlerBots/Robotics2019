f1900bfb70f5eee2fa980c4750f0100dd1f842c0
Jasonface900 <jorge.floresm900@gmail.com>
2019-01-19 08:40:20 -0800

    fix'd etan's issues lol

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * This is the constants interface. This is used for easy changing of ports, controller buttons, and other things
+ * this file should be well commented and documented for ease of use by non-programmers
+ */
+public interface Constants {
+
+}
